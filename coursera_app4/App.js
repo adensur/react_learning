cfb383bc1541644b1462bd0ce5a65dc6a5f935d2
adensur <adensur@gmail.com>
2022-11-21 23:22:45 +0000

    asd

--- coursera_app4/App.js
+++ coursera_app4/App.js
@@ -2,34 +2,44 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MenuScreen from "./Screens/MenuScreen";
 import WelcomeScreen from "./Screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Welcome"
-        screenOptions={{ headerStyle: { backgroundColor: "#FBDABB" } }}
-      >
-        <Stack.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{ title: "Home" }}
-        />
-        <Stack.Screen name="Menu" component={MenuScreen} />
-      </Stack.Navigator>
+      <View style={styles.container}>
+        <Tab.Navigator>
+          <Tab.Screen name="tab1" component={Tab1} />
+          <Tab.Screen name="tab2" component={Tab2} />
+        </Tab.Navigator>
+      </View>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    flex: 1
   }
 });
+
+function Tab1() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Tab1</Text>
+    </View>
+  );
+}
+
+function Tab2() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Tab2</Text>
+    </View>
+  );
+}
--- coursera_app4/Screens/MenuScreen.js
+++ coursera_app4/Screens/MenuScreen.js
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
 
 export default function MenuScreen() {
   return (
     <View>
-      <Text>Hello World!</Text>
+      <Tab1 />
+      <Tab2 />
     </View>
   );
 }
