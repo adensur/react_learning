a9388916a1d0be1c6e919195b8de6d31514a9649
adensur <adensur@gmail.com>
2022-12-09 14:30:42 +0000

    Spotify api auth

--- next_js/chakra/pages/api/search.tsx
+++ next_js/chakra/pages/api/search.tsx
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { request } from "http";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -9,5 +10,30 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log("Got request!");
+  //
+  var client_id = "3df16b4f97464a6dacbb72867417bd5e";
+  var client_secret = "ffdb57ad64f9441aafbd4faac7c14045";
+  console.log(
+    "Auth: ",
+    Buffer.from(client_id + ":" + client_secret).toString("base64")
+  );
+
+  fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret).toString("base64")
+    },
+    body: "grant_type=client_credentials"
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch(function (error) {
+      console.log("Got error: ", error);
+    });
   res.status(200).json({ content: "You searched for: " + req.query.text });
 }
