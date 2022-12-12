0b9d0fb99dfc6ded0c5f1967d3f2eef134c0ec23
adensur <adensur@gmail.com>
2022-12-12 12:53:44 +0000

    actual search api

--- next_js/chakra/pages/api/search.tsx
+++ next_js/chakra/pages/api/search.tsx
@@ -2,24 +2,31 @@
 import { request } from "http";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  content: string;
+type Track = {
+  name: string;
+  artists: string;
+  id: string;
 };
 
-export default function handler(
+type Data = {
+  content: string;
+  tracks: Array<Track>;
+};
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   console.log("Got request!");
   //
-  var client_id = "3df16b4f97464a6dacbb72867417bd5e";
-  var client_secret = "ffdb57ad64f9441aafbd4faac7c14045";
+  let client_id = "3df16b4f97464a6dacbb72867417bd5e";
+  let client_secret = "ffdb57ad64f9441aafbd4faac7c14045";
   console.log(
     "Auth: ",
     Buffer.from(client_id + ":" + client_secret).toString("base64")
   );
 
-  fetch("https://accounts.spotify.com/api/token", {
+  let token = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
@@ -31,9 +38,38 @@ export default function handler(
     body: "grant_type=client_credentials"
   })
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      return data.access_token;
+    })
     .catch(function (error) {
       console.log("Got error: ", error);
     });
-  res.status(200).json({ content: "You searched for: " + req.query.text });
+  console.log("Access token: ", token, typeof token);
+  let resp = await fetch(
+    "https://api.spotify.com/v1/search?type=track,artist&q=" + req.query.text,
+    {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    }
+  ).then((response) => response.json());
+  //console.log("Got resp: ", resp);
+  const tracks_unpacked = resp.tracks.items.map((track: any) => {
+    return {
+      name: track.name,
+      artists: track.artists.map((artist: any) => artist.name).join(", "),
+      id: track.id
+    };
+  });
+  for (const track of resp.tracks.items) {
+    console.log(
+      "Track: ",
+      track.name,
+      track.artists.map((artist: any) => artist.name).join(", ")
+    );
+  }
+  res.status(200).json({
+    content: "You searched for: " + req.query.text,
+    tracks: tracks_unpacked
+  });
 }
