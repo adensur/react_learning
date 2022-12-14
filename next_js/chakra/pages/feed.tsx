f91a6159844f3b316bdd6b4bf83606af5f3e02e3
adensur <adensur@gmail.com>
2022-12-14 13:05:46 +0000

    display feed

--- next_js/chakra/pages/feed.tsx
+++ next_js/chakra/pages/feed.tsx
@@ -11,9 +11,30 @@ import AddSongModal2 from "../components/add_song2";
 import type { Track, ExtendedTrack } from "../utils/data";
 import useAuth from "../utils/auth";
 
+function Tracks({ tracks }: { tracks: Array<ExtendedTrack> }) {
+  return (
+    <>
+      {tracks.map((track: ExtendedTrack, idx: number) => {
+        return (
+          <Text key={idx}>
+            {track.track.artists +
+              ": " +
+              track.track.name +
+              "; note: " +
+              track.note +
+              "; state: " +
+              track.state}
+          </Text>
+        );
+      })}
+    </>
+  );
+}
+
 export default function Feed() {
   const uid = useAuth();
-  let [userSnapshot, setUserSnapshot] = useState<any | null>(null);
+  //let [userSnapshot, setUserSnapshot] = useState<any | null>(null);
+  let [tracks, setTracks] = useState<Array<ExtendedTrack>>([]);
   const db = getDatabase(app);
   useEffect(() => {
     // set up firebase db listener
@@ -22,15 +43,21 @@ export default function Feed() {
       const data = snapshot.val();
       if (snapshot.exists()) {
         console.log("Data: ", data);
-        console.log("User Snapshot: ", userSnapshot);
-        if (!_.isEqual(data, userSnapshot)) {
+        //console.log("User Snapshot: ", userSnapshot);
+        let tracks2 = [];
+        for (const trackId in data.tracks) {
+          tracks2.push(data.tracks[trackId]);
+        }
+        console.log("Tracks 2: ", tracks2);
+        if (!_.isEqual(tracks, tracks2)) {
           console.log("Data != user snapshot!");
-          setUserSnapshot(data);
+          //setUserSnapshot(data);
+          setTracks(tracks2);
         }
       }
     });
   });
-  console.log("Got user snapshot: ", userSnapshot);
+  //console.log("Got user snapshot: ", userSnapshot);
   const {
     isOpen: isOpenAddSong,
     onOpen: onOpenAddSong,
@@ -42,19 +69,15 @@ export default function Feed() {
     onOpen: onOpenAddSong2,
     onClose: onCloseAddSong2
   } = useDisclosure();
-  if (uid) {
+  console.log("Tracks: ", tracks);
+  if (uid && tracks.length > 0) {
     return (
       <>
         <Layout>
           <Text>Welcome, {uid}</Text>
+          <Tracks tracks={tracks} />
           <Button onClick={onOpenAddSong}>Add Song</Button>
         </Layout>
-        {
-          //userSnapshot &&
-          // userSnapshot!!.tracks.map((track: any, idx: number) => (
-          //  <Text key={idx}>{track}</Text>
-          //))
-        }
         <AddSongModal
           isOpen={isOpenAddSong}
           onClose={onCloseAddSong}
